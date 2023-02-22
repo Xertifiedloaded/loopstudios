4bed2d35b1f5bdef269eed8d01afb36b4d3be17f
xertifiedloaded <certifiedloaded@yahoo.com>
2023-02-22 12:07:47 +0100

    mobile hambugger

--- src/components/ui/header/header.js
+++ src/components/ui/header/header.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from '../header/header.module.css'
 import Logo from '../../../assets/images/mobile/logo.svg'
 import { NavLink } from 'react-router-dom'
+import HamburgerOpen from '../../../assets/images/desktop/icon-hamburger.svg'
+import HamburgerClose  from '../../../assets/images/desktop/icon-close.svg'
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(false)
+
     return (
         <>
+            {/* mobile hamburger */}
             <div className={classes.main}>
                 <div className={classes.logo}>
                     <img src={Logo} alt="logo" srcset="" />
                 </div>
                 <nav>
-                    <NavLink>About</NavLink>
-                    <NavLink>Careers</NavLink>
-                    <NavLink>Events</NavLink>
-                    <NavLink>Products</NavLink>
-                    <NavLink>Supports</NavLink>
+                    <div className={classes.mobileIcon} onClick={() => setIsMobile(!isMobile)}>
+                        {isMobile ? <img src={HamburgerClose} /> : <img src={HamburgerOpen} />}
+                    </div>
+                    <ul className={`${isMobile ? classes.open : classes.close}`} onClick={()=>setIsMobile(isMobile)}>
+                        <NavLink>About</NavLink>
+                        <NavLink>Careers</NavLink>
+                        <NavLink>Events</NavLink>
+                        <NavLink>Products</NavLink>
+                        <NavLink>Supports</NavLink>
+                    </ul>
                 </nav>
+               
             </div>
         </>
     )
